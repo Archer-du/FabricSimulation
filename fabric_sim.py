62e8b716794eca24e5bdc4d5267d805be21baf30
Archer <dupengche@gmail.com>
2023-06-25 14:30:17 +0800

    basic function completed

--- fabric_sim.py
+++ fabric_sim.py
@@ -3,219 +3,210 @@ import taichi as ti
 
 ti.init(arch=ti.vulkan)
 
-# GLOBAL CONFIG ===============================
-# fabric arguments
-n = 128
-mass = 1.0
-quad_size = 1.0 / n
-x = ti.Vector.field(3, dtype=float, shape=(n, n))
-v = ti.Vector.field(3, dtype=float, shape=(n, n))
 
-spring_offsets = []
-for i in range(-2, 3):
-    for j in range(-2, 3):
-        if (i, j) != (0, 0) and abs(i) + abs(j) <= 2:
-            spring_offsets.append(ti.Vector([i, j]))
-
-# spring arguments
-stiffness = 1e6
-damping = 1e2
-
-# collider arguments
-ball_radius = 0.3
-ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
-ball_center[0] = [0, 0, 0]
-ball_stiffness = 1e5
-
-# environment arguments
-gravity = ti.Vector([0, -9.8, 0])
-drag_damping = 1
-
-# system arguments
-dt = 4e-2 / n
-substeps = int(1 / 60 // dt)
-num_triangles = (n - 1) * (n - 1) * 2
-
-# render arguments
-indices = ti.field(int, shape=num_triangles * 3)
-vertices = ti.Vector.field(3, dtype=float, shape=n * n)
-colors = ti.Vector.field(3, dtype=float, shape=n * n)
-
-
-@ti.dataclass
-class MassPoint:
-    def __init__(self, mass: ti.f32 = 1.0):
-        self.mass = mass
-        self.position = ti.Vector([0, 0, 0])
-        self.velocity = ti.Vector([0, 0, 0])
-
-@ti.dataclass
-class Spring:
-    def __init__(self, stiffness = 1e6, damping = 1e2):
-        self.stiffness = stiffness
-        self.damping = damping
-
-@ti.dataclass
+# DATA CLASS ==================================
+@ti.data_oriented
 class Fabric:
-    def __init__(self, massNum: ti.i32 = 128, length: ti.f32 = 1.0):
+    
+    def __init__(self, mass: ti.f32 = 1.0, massNum: ti.i32 = 128, length: ti.f32 = 1.25):
+        # simulation arguments
+        self.mass = mass
         self.massNum = massNum
-        self.massPoints = ti.field(dtype=MassPoint, shape=(massNum, massNum))
         self.length = length
         self.quadSize = length / (massNum - 1)
         self.offsets = []
+        self.position = ti.Vector.field(3, dtype=ti.f32, shape=(massNum, massNum))
+        self.velocity = ti.Vector.field(3, dtype=ti.f32, shape=(massNum, massNum))
+        self.initSpringOffset()
+        self.InitMassPoints()
+
+        # render arguments
+        triangleNum = (massNum - 1) * (massNum - 1) * 2
+        self.triangleIndices = ti.field(ti.i32, shape=triangleNum * 3)
+        self.vertices = ti.Vector.field(3, dtype=ti.f32, shape=massNum * massNum)
+        self.colors = ti.Vector.field(3, dtype=ti.f32, shape=massNum * massNum)
+        self.InitTriangleMesh()
+
+    def initSpringOffset(self):
         for i in range(-2, 3):
             for j in range(-2, 3):
                 if (i, j) != (0, 0) and abs(i) + abs(j) <= 2:
                     self.offsets.append(ti.Vector([i, j]))
-        #self.position = ti.Vector.field(3, dtype=ti.f32, shape=(massNum, massNum))
-        #self.velocity = ti.Vector.field(3, dtype=ti.f32, shape=(massNum, massNum))
 
-@ti.dataclass
+    # SIM INIT -------------
+    @ti.kernel
+    def InitMassPoints(self):
+        random_offset = ti.Vector([ti.random() - 0.5, ti.random() - 0.5]) * 0.1
+
+        for i, j in self.position:
+            self.position[i, j] = [
+                i * self.quadSize - self.length / 2 + random_offset[0], 0.6,
+                j * self.quadSize - self.length / 2 + random_offset[1]
+            ]
+            self.velocity[i, j] = [0, 0, 0]
+
+    # SIM UPDATE -----------
+    @ti.kernel
+    def UpdateSys(self):
+        # environment adjustment
+        for i in ti.grouped(self.position):
+            # update velocity: gravity
+            self.velocity[i] += GRAVITY * dt
+            # update velocity: air DAMPING
+            self.velocity[i] *= ti.exp(-AIR_DRAG * dt)
+
+        # Symplectic Euler analysis
+        for i in ti.grouped(self.position):
+            force = ti.Vector([0.0, 0.0, 0.0])
+
+            for springOffset in ti.static(self.offsets):
+                j = i + springOffset
+                if 0 <= j[0] < self.massNum and 0 <= j[1] < self.massNum:
+
+                    # relative displacement
+                    x_ij = self.position[i] - self.position[j]
+                    # relative movement
+                    v_ij = self.velocity[i] - self.velocity[j]
+
+                    # vector normalization
+                    e_ij = x_ij.normalized()
+                    # modulus length
+                    cur_len = x_ij.norm()
+                    ori_len = self.quadSize * float(i - j).norm()
+
+                    # coefficient balance
+                    coe = (self.quadSize / ori_len)
+                    k_s = STIFFNESS * coe
+                    k_d = DAMPING
+
+                    # spring force
+                    force += -k_s * e_ij * (cur_len - ori_len)
+                    # spring damping
+                    force += -k_d * e_ij * v_ij.dot(e_ij)
+
+            self.velocity[i] += (force / self.mass) * dt
+
+        # collision detection
+        for i in ti.grouped(self.position):
+            offset_to_center = self.position[i] - sphere.center[0]
+            if offset_to_center.norm() <= sphere.radius:
+                e = offset_to_center.normalized()
+                self.velocity[i] -= min(self.velocity[i].dot(e), 0) * e
+
+            # update position
+            self.position[i] += dt * self.velocity[i]
+
+    # RENDER INIT -------------
+    @ti.kernel
+    def InitTriangleMesh(self):
+        for i, j in ti.ndrange(self.massNum - 1, self.massNum - 1):
+            quad_id = (i * (self.massNum - 1)) + j
+            # 1st triangle of the square
+            self.triangleIndices[quad_id * 6 + 0] = i * self.massNum + j
+            self.triangleIndices[quad_id * 6 + 1] = (i + 1) * self.massNum + j
+            self.triangleIndices[quad_id * 6 + 2] = i * self.massNum + (j + 1)
+            # 2nd triangle of the square
+            self.triangleIndices[quad_id * 6 + 3] = (i + 1) * self.massNum + j + 1
+            self.triangleIndices[quad_id * 6 + 4] = i * self.massNum + (j + 1)
+            self.triangleIndices[quad_id * 6 + 5] = (i + 1) * self.massNum + j
+
+        for i, j in ti.ndrange(self.massNum, self.massNum):
+            if (i // 4 + j // 4) % 2 == 0:
+                self.colors[i * self.massNum + j] = (0.22, 0.72, 0.52)
+            else:
+                self.colors[i * self.massNum + j] = (1, 0.334, 0.52)
+
+    # RENDER UPDATE -----------
+    @ti.kernel
+    def UpdateVertices(self):
+        for i, j in ti.ndrange(self.massNum, self.massNum):
+            self.vertices[i * self.massNum + j] = self.position[i, j]
+        
+
+@ti.data_oriented
 class Collider:
     def __init__(self, radius: ti.f32 = 0.3, center: ti.Vector = [0, 0, 0]):
+        # simulation arguments
         self.radius = radius
         self.center = ti.Vector.field(3, dtype=float, shape=(1, ))
         self.center[0] = center
 
 
-@ti.kernel
-def initialize_mass_points():
-    random_offset = ti.Vector([ti.random() - 0.5, ti.random() - 0.5]) * 0.1
-    #random_offset = ti.Vector([0, 0])
+# GLOBAL CONFIG ===============================
+# fabric config
+fabric1 = Fabric(massNum=64)
+fabric2 = Fabric(massNum=128)
+fabric3 = Fabric(massNum=16)
+sphere = Collider()
 
-    for i, j in x:
-        x[i, j] = [
-            i * quad_size - 0.5 + random_offset[0], 0.6,
-            j * quad_size - 0.5 + random_offset[1]
-        ]
-        v[i, j] = [0, 0, 0]
+# spring config
+STIFFNESS = 1e6
+DAMPING = 1e2
 
+# environment arguments
+GRAVITY = ti.Vector([0, -9.8, 0])
+AIR_DRAG = 1.0
 
-# UPDATE ======================================
-@ti.kernel
-def substep():
-
-    # environment adjustment
-    for i in ti.grouped(x):
-        # update velocity: gravity
-        v[i] += gravity * dt
-        # update velocity: air damping
-        v[i] *= ti.exp(-drag_damping * dt)
+# system arguments
+dt = 1 / 3200
+substeps = int(1 / 60 // dt)
 
 
-    # Symplectic Euler analysis
-    for i in ti.grouped(x):
-        force = ti.Vector([0.0, 0.0, 0.0])
-
-        for spring_offset in ti.static(spring_offsets):
-            j = i + spring_offset
-            if 0 <= j[0] < n and 0 <= j[1] < n:
-
-                # relative displacement
-                x_ij = x[i] - x[j]
-                # relative movement
-                v_ij = v[i] - v[j]
-
-                # vector normalization
-                e_ij = x_ij.normalized()
-                # modulus length
-                cur_len = x_ij.norm()
-                ori_len = quad_size * float(i - j).norm()
-
-                # coefficient balance
-                coe = (quad_size / ori_len)
-                k_s = stiffness * coe
-                k_d = damping
-
-                # spring force
-                force += -k_s * e_ij * (cur_len - ori_len)
-                # spring damping
-                force += -k_d * e_ij * v_ij.dot(e_ij)
-
-        v[i] += (force / mass) * dt
-
-    # collision detection
-    for i in ti.grouped(x):
-        offset_to_center = x[i] - ball_center[0]
-        if offset_to_center.norm() <= ball_radius:
-            normal = offset_to_center.normalized()
-            v[i] -= min(v[i].dot(normal), 0) * normal
-
-        # update position
-        x[i] += dt * v[i]
-        
-
-# RENDER ======================================
-@ti.kernel
-def initialize_mesh_indices():
-    for i, j in ti.ndrange(n - 1, n - 1):
-        quad_id = (i * (n - 1)) + j
-        # 1st triangle of the square
-        indices[quad_id * 6 + 0] = i * n + j
-        indices[quad_id * 6 + 1] = (i + 1) * n + j
-        indices[quad_id * 6 + 2] = i * n + (j + 1)
-        # 2nd triangle of the square
-        indices[quad_id * 6 + 3] = (i + 1) * n + j + 1
-        indices[quad_id * 6 + 4] = i * n + (j + 1)
-        indices[quad_id * 6 + 5] = (i + 1) * n + j
-
-    for i, j in ti.ndrange(n, n):
-        if (i // 4 + j // 4) % 2 == 0:
-            colors[i * n + j] = (0.22, 0.72, 0.52)
-        else:
-            colors[i * n + j] = (1, 0.334, 0.52)
-
-
-@ti.kernel
-def update_vertices():
-    for i, j in ti.ndrange(n, n):
-        vertices[i * n + j] = x[i, j]
-
-
-fabric = Fabric()
-initialize_mass_points()
-
-# GGUI settings
+# GGUI SETTINGS ================================
 window = ti.ui.Window("Fabric Simulation", (1600, 1600))
 canvas = window.get_canvas()
 canvas.set_background_color((0, 0, 0))
-
 scene = ti.ui.Scene()
 camera = ti.ui.Camera()
+camera.position(0.0, 0.0, 3)
+camera.lookat(0.0, 0.0, 0)
+scene.set_camera(camera)
 
-current_t = 0.0
-initialize_mesh_indices()
-
+fabric = fabric3
+currentTime = 0.0
+skeletion = False
 while window.running:
+    # global render
+    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
+    scene.ambient_light((0.5, 0.5, 0.5))
+    scene.particles(sphere.center, radius=sphere.radius * 0.95, color=(0.5, 0.42, 0.8))
+    if skeletion:
+        print("skel")
+    else:
+        fabric.UpdateVertices()
+        scene.mesh(fabric.vertices,
+                indices=fabric.triangleIndices,
+                per_vertex_color=fabric.colors,
+                two_sided=True)
+
+    # keyboard operation
     if window.get_event(ti.ui.PRESS):
         key = window.event.key
         if key == "q":
             exit()
-        if key == "k":
-            n = 16
-    if current_t > 2.0:
-        # Reset
-        initialize_mass_points()
-        current_t = 0
+        if key == "c":
+            skeletion = not skeletion
+        if key == "u":
+            fabric = fabric1
+            currentTime = 0
 
+        if key == "i":
+            fabric = fabric2
+            currentTime = 0
+
+        if key == "o":
+            fabric = fabric3
+            currentTime = 0
+    
+    # reset
+    if currentTime > 2.0:
+        fabric.InitMassPoints()
+        currentTime = 0
+
+    # iteration
     for i in range(substeps):
-        substep()
-        current_t += dt
-    update_vertices()
+        fabric.UpdateSys()
+        currentTime += dt
 
-    camera.position(0.0, 0.0, 3)
-    camera.lookat(0.0, 0.0, 0)
-    scene.set_camera(camera)
-
-    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
-    scene.ambient_light((0.5, 0.5, 0.5))
-    scene.mesh(vertices,
-               indices=indices,
-               per_vertex_color=colors,
-               two_sided=True)
-
-    # Draw a smaller ball to avoid visual penetration
-    scene.particles(ball_center, radius=ball_radius * 0.95, color=(0.5, 0.42, 0.8))
     canvas.scene(scene)
-
     window.show()
--- test.py
+++ test.py
@@ -1,145 +1,63 @@
 import taichi as ti
-ti.init(arch=ti.vulkan)  # Alternatively, ti.init(arch=ti.cpu)
 
-n = 128
-quad_size = 1.0 / n
-dt = 4e-2 / n
-substeps = int(1 / 60 // dt)
+ti.init(arch=ti.vulkan)
 
-gravity = ti.Vector([0, -9.8, 0])
-spring_Y = 3e4
-dashpot_damping = 1e4
-drag_damping = 1
+# 设置网格的大小和间隔
+grid_size = 10
+grid_step = 0.1
 
-ball_radius = 0.3
-ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
-ball_center[0] = [0, 0, 0]
+# 创建一个Taichi 3D向量场，用来存储网格的顶点位置
+vertices = ti.Vector.field(3, dtype=ti.f32, shape=(grid_size + 1) ** 2)
 
-x = ti.Vector.field(3, dtype=float, shape=(n, n))
-v = ti.Vector.field(3, dtype=float, shape=(n, n))
-
-num_triangles = (n - 1) * (n - 1) * 2
-indices = ti.field(int, shape=num_triangles * 3)
-vertices = ti.Vector.field(3, dtype=float, shape=n * n)
-colors = ti.Vector.field(3, dtype=float, shape=n * n)
-
-bending_springs = False
-
+# 遍历每个顶点，计算其位置
 @ti.kernel
-def initialize_mass_points():
-    random_offset = ti.Vector([ti.random() - 0.5, ti.random() - 0.5]) * 0.1
+def init_vertices():
+    for i, j in ti.ndrange(grid_size + 1, grid_size + 1):
+        x = i * grid_step - 0.5
+        y = j * grid_step - 0.5
+        z = 0.0
+        vertices[i * (grid_size + 1) + j] = [x, y, z]
 
-    for i, j in x:
-        x[i, j] = [
-            i * quad_size - 0.5 + random_offset[0], 0.6,
-            j * quad_size - 0.5 + random_offset[1]
-        ]
-        v[i, j] = [0, 0, 0]
+# 调用init_vertices函数
+init_vertices()
 
+# 创建一个Taichi整数场，用来存储网格的线段索引
+indices = ti.field(ti.i32, shape=grid_size * (grid_size + 1) * 2)
 
+# 遍历每条线段，计算其索引
 @ti.kernel
-def initialize_mesh_indices():
-    for i, j in ti.ndrange(n - 1, n - 1):
-        quad_id = (i * (n - 1)) + j
-        # 1st triangle of the square
-        indices[quad_id * 6 + 0] = i * n + j
-        indices[quad_id * 6 + 1] = (i + 1) * n + j
-        indices[quad_id * 6 + 2] = i * n + (j + 1)
-        # 2nd triangle of the square
-        indices[quad_id * 6 + 3] = (i + 1) * n + j + 1
-        indices[quad_id * 6 + 4] = i * n + (j + 1)
-        indices[quad_id * 6 + 5] = (i + 1) * n + j
+def init_indices():
+    # 网格的水平线段
+    for i, j in ti.ndrange(grid_size + 1, grid_size):
+        k = i * grid_size + j
+        indices[k * 2] = i * (grid_size + 1) + j
+        indices[k * 2 + 1] = i * (grid_size + 1) + j + 1
 
-    for i, j in ti.ndrange(n, n):
-        if (i // 4 + j // 4) % 2 == 0:
-            colors[i * n + j] = (0.22, 0.72, 0.52)
-        else:
-            colors[i * n + j] = (1, 0.334, 0.52)
+    # 网格的垂直线段
+    for i, j in ti.ndrange(grid_size, grid_size + 1):
+        k = (grid_size + 1) * grid_size + i * grid_size + j
+        indices[k * 2] = i * (grid_size + 1) + j
+        indices[k * 2 + 1] = (i + 1) * (grid_size + 1) + j
 
-initialize_mesh_indices()
+# 调用init_indices函数
+init_indices()
 
-spring_offsets = []
-if bending_springs:
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if (i, j) != (0, 0):
-                spring_offsets.append(ti.Vector([i, j]))
-
-else:
-    for i in range(-2, 3):
-        for j in range(-2, 3):
-            if (i, j) != (0, 0) and abs(i) + abs(j) <= 2:
-                spring_offsets.append(ti.Vector([i, j]))
-
-@ti.kernel
-def substep():
-    for i in ti.grouped(x):
-        v[i] += gravity * dt
-
-    for i in ti.grouped(x):
-        force = ti.Vector([0.0, 0.0, 0.0])
-        for spring_offset in ti.static(spring_offsets):
-            j = i + spring_offset
-            if 0 <= j[0] < n and 0 <= j[1] < n:
-                x_ij = x[i] - x[j]
-                v_ij = v[i] - v[j]
-                d = x_ij.normalized()
-                current_dist = x_ij.norm()
-                original_dist = quad_size * float(i - j).norm()
-                # Spring force
-                force += -spring_Y * d * (current_dist / original_dist - 1)
-                # Dashpot damping
-                force += -v_ij.dot(d) * d * dashpot_damping * quad_size
-
-        v[i] += force * dt
-
-    for i in ti.grouped(x):
-        v[i] *= ti.exp(-drag_damping * dt)
-        offset_to_center = x[i] - ball_center[0]
-        if offset_to_center.norm() <= ball_radius:
-            # Velocity projection
-            normal = offset_to_center.normalized()
-            v[i] -= min(v[i].dot(normal), 0) * normal
-        x[i] += dt * v[i]
-
-@ti.kernel
-def update_vertices():
-    for i, j in ti.ndrange(n, n):
-        vertices[i * n + j] = x[i, j]
-
-window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (2000, 2000),
-                      vsync=True)
+window = ti.ui.Window("Fabric Simulation", (1600, 1600))
 canvas = window.get_canvas()
 canvas.set_background_color((0, 0, 0))
 scene = ti.ui.Scene()
 camera = ti.ui.Camera()
+camera.position(0.0, 0.0, 3)
+camera.lookat(0.0, 0.0, 0)
+scene.set_camera(camera)
 
-current_t = 0.0
-initialize_mass_points()
 
 while window.running:
-    if current_t > 1.5:
-        # Reset
-        initialize_mass_points()
-        current_t = 0
+    # 在scene中声明线段，用白色表示网格
+    scene.lines(vertices, width=2.0, color=(1.0, 1.0, 1.0))
 
-    for i in range(substeps):
-        substep()
-        current_t += dt
-    update_vertices()
-
-    camera.position(0.0, 0.0, 3)
-    camera.lookat(0.0, 0.0, 0)
-    scene.set_camera(camera)
-
-    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
-    scene.ambient_light((0.5, 0.5, 0.5))
-    scene.mesh(vertices,
-               indices=indices,
-               per_vertex_color=colors,
-               two_sided=True)
-
-    # Draw a smaller ball to avoid visual penetration
-    scene.particles(ball_center, radius=ball_radius * 0.95, color=(0.5, 0.42, 0.8))
-    canvas.scene(scene)
+    # 在scene中声明一个红色的点，用来表示原点
+    center = ti.Vector.field(3, dtype=float, shape=(1, ))
+    center[0] = ti.Vector([0, 0, 0])
+    scene.particles(center, radius=5.0, color=(1.0, 0.0, 0.0))
     window.show()
